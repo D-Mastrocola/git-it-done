a77f3ecafa0526967c348e68cd031bf2ea9233f6
Dominic Mastrocola <60686549+D-Mastrocola@users.noreply.github.com>
2021-09-14 11:54:05 -0400

    made the API call dynamic

--- assets/js/homepage.js
+++ assets/js/homepage.js
@@ -1,4 +1,14 @@
-var getUserRepos = function () {
-  fetch("https://api.github.com/users/octocat/repos");
+var getUserRepos = function (user) {
+  //format the url based on user
+  let apiUrl = "https://api.github.com/users/" + user + "/repos";
+
+  //then make an api call
+  fetch(apiUrl).then(function (response) {
+    response.json().then(function (data) {
+      console.log(data);
+    });
+  });
+
+  console.log("outside");
 };
-getUserRepos();
+getUserRepos('d-mastrocola');
